90ef3863424f911fcf88d5286ef42fbbaa8ed473
David Fernandes <devdkingg@gmail.com>
2024-10-07 23:43:22 -0300

    feat: authentication feature start

--- app/api/auth/route.tsx
+++ app/api/auth/route.tsx
@@ -1,19 +1,39 @@
-// import { NextResponse } from 'next/server';
-// import { PrismaClient } from '@prisma/client';
-// import { ownerSchema } from '../../../utils/validation';
-// import { z } from 'zod';
+import { NextResponse } from 'next/server';
+import { PrismaClient } from '@prisma/client';
+import { ownerSchema } from '../../../utils/validation';
+import { z } from 'zod';
+import jwt from "jsonwebtoken"
+import bcrypt from "bcrypt"
 
-// const prisma = new PrismaClient();
+const prisma = new PrismaClient();
 
-// export async function POST(request: Request) {
-//   try {
-//     const body = await request.json();
-//     return NextResponse.json(body, { status: 201 });
-//   } catch (error) {
-//     if (error instanceof z.ZodError) {
-//       return NextResponse.json({ error: error.errors }, { status: 400 });
-//     }
-//     console.log( error instanceof Error ? error.message : null)
-//     return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 });
-//   }
-// }
+export async function POST(request: Request) {
+  const generateToken = (id: string) => jwt.sign({ id: id }, process.env.SECRET_JWT, { expiresIn: 86400 });
+
+  try {
+    const { email, password } = await request.json();
+
+    if (!email || !password) {
+      return NextResponse.json({ error: 'Email and password is required' }, { status: 400 });
+    }
+
+    const foundUser = await prisma.owner.findUnique({
+      where: { email }
+    });
+
+    if (!foundUser) {
+      return NextResponse.json({ error: 'Icorrect E-mail or password' }, { status: 500 });
+    }
+
+    if (!await bcrypt.compare(password, foundUser.password)) {
+      return NextResponse.json({ bcrypt: await bcrypt.compare(password, foundUser.password), password: password, fupassword: foundUser.password, message: "Incorrect E-mail or password" }, { status: 500 });
+    }
+
+    const atk = generateToken(foundUser.id);
+    return NextResponse.json({ atk, message: "Used logged successfully" }, { status: 200 });
+
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json({ error: 'Internal server error' }, { status: 500 });
+  }
+}
--- app/api/owners/route.ts
+++ app/api/owners/route.ts
@@ -2,17 +2,28 @@ import { NextResponse } from 'next/server';
 import { PrismaClient } from '@prisma/client';
 import { ownerSchema } from '../../../utils/validation';
 import { z } from 'zod';
+import jwt from "jsonwebtoken"
+import bcrypt from "bcrypt"
 
 const prisma = new PrismaClient();
 
-export async function POST(request: Request) {
+export async function POST(request: Request) { 
+  const generateToken = (id: string) => jwt.sign({ id: id }, process.env.SECRET_JWT, { expiresIn: 86400 });
+
   try {
     const body = await request.json();
     const validatedOwner = ownerSchema.parse(body);
+
+    const foundUser = await prisma.owner.findUnique({where: {email: body.email}})
+    if (foundUser) return NextResponse.json({message: "E-mail já cadastrado"},{status: 400})
+
+    const hashedPassword = await bcrypt.hash(body.password, 7)
     const newOwner = await prisma.owner.create({
-      data: validatedOwner,
+      data: {...validatedOwner, password: hashedPassword },
     });
-    return NextResponse.json(newOwner, { status: 201 });
+
+    const atk = await generateToken(newOwner.id)
+    return NextResponse.json({ atk, message: "User created successfully"}, { status: 201 });
   } catch (error) {
     if (error instanceof z.ZodError) {
       return NextResponse.json({ error: error.errors }, { status: 400 });
